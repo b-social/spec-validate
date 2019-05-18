a66086e5160cb001f6b0a95b34e79c8d52ac9e21
Toby Clemson <tobyclemson@gmail.com>
2019-05-18 17:22:53 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spec-validate "0.1.0"
+(defproject spec-validate "0.1.1-SNAPSHOT"
   :description "A clojure.spec based validation library."
   :url "https://github.com/b-social/spec-validate"
   :license {:name "The MIT License"
