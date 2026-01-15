61a51cd8131ecc0a4b81ce3d60842adcc60308fa
Mohamed Yusuf <66272875+mhyusuf@users.noreply.github.com>
2026-01-15 11:42:21 +0000

    [CAPAY-592] Use minimum Clojure version supporting `parse-uuid`

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
             [lein-codox "0.10.7"]
             [lein-cljfmt "0.6.4"]]
 
-  :profiles {:shared {:dependencies [[org.clojure/clojure "1.12.4"]
+  :profiles {:shared {:dependencies [[org.clojure/clojure "1.11.2"]
                                      [eftest "0.5.8"]]}
              :dev    [:shared]
              :test   [:shared]}
